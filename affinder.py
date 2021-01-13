1baed98051e706ea787942f7ac2570763c2e5ab9
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-01-13 15:58:24 +1100

    Adding points works, but affine not getting updated

--- affinder.py
+++ affinder.py
@@ -11,7 +11,7 @@ from skimage.transform import AffineTransform
 
 
 @tz.curry
-def next_layer_callback(viewer, event, *, image_layer0, pts_layer0, image_layer1, pts_layer1):
+def next_layer_callback(value, *args, viewer, event, image_layer0, pts_layer0, image_layer1, pts_layer1):
     pts0, pts1 = pts_layer0.data, pts_layer1.data
     n0, n1 = len(pts0), len(pts1)
     if pts_layer0.selected:
@@ -35,11 +35,12 @@ def next_layer_callback(viewer, event, *, image_layer0, pts_layer0, image_layer1
             viewer.layers.move(viewer.layers.index(pts_layer0), -1)
         
 
-@magicgui(call_button='Start', layout='vertical', viewer={'visible': False, 'label': ''})
+@magicgui(call_button='Start', layout='vertical', viewer={'visible': False, 'label': ' '})
 def start_affinder(reference: napari.layers.Image, moving: napari.layers.Image, viewer : napari.Viewer):
     # make a points layer for each image
     points_layers = []
-    for layer in [reference, moving]:
+    # Use C0 and C1 from matplotlib color cycle
+    for layer, color in [(reference, (0.122, 0.467, 0.706, 1.0)), (moving, (1.0, 0.498, 0.055, 1.0))]:
         data = (layer.data
                 if not isinstance(layer.data, list)
                 else layer.data[0]
@@ -47,28 +48,43 @@ def start_affinder(reference: napari.layers.Image, moving: napari.layers.Image, 
         new_layer = napari.layers.Points(
                 ndim=data.ndim, name=layer.name + '_pts'
                 )
+        new_layer.current_face_color = color
         viewer.layers.append(new_layer)
         points_layers.append(new_layer)
     pts_layer0 = points_layers[0]
     pts_layer1 = points_layers[1]
     
+    # make a callback for points added
     callback = next_layer_callback(
+        viewer=viewer,
+        event=None,
         image_layer0=reference,
         pts_layer0=pts_layer0,
         image_layer1=moving,
         pts_layer1=pts_layer1,
         )
-    viewer.mouse_drag_callbacks.append(callback)
+    pts_layer0.events.data.connect(callback)
+    pts_layer1.events.data.connect(callback)
+    close_affinder.layers.value = [pts_layer0, pts_layer1]
     close_affinder.callback.value = callback
+
+    # get the layer order started
+    for layer in [moving, pts_layer1, reference, pts_layer0]:
+        viewer.layers.move(viewer.layers.index(layer), -1)
+    
+    viewer.layers.unselect_all()
+    pts_layer0.selected = True
+    pts_layer0.mode = 'add'
 
 
 @magicgui(
         call_button='Finish',
-        callback={'visible': False, 'label': ''},
-        viewer={'visible': False, 'label': ''}
+        callback={'visible': False, 'label': ' '},
+        layers={'visible': False, 'label': ' '}
         )
-def close_affinder(viewer: napari.Viewer, callback=None):
-    viewer.mouse_drag_callbacks.remove(callback)
+def close_affinder(layers, callback=None):
+    for layer in layers:
+        layer.events.data.disconnect(callback)
 
 
 # affinder = Container(widgets=[start_affinder, close_affinder], layout='vertical')
